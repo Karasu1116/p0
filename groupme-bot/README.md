5620dacfaa520bd1e881ee8e8241e667b4b8ab82
Greg Dellamura <g.dellamura@Gregs-MacBook-Pro-2.local>
2024-01-09 21:51:46 -0500

    completed p0

---
+++ groupme-bot/README.md
@@ -0,0 +1,14 @@
+# GBOT389 : CMSC389T Winter 2024
+
+# How to run
+
+1. source venv/bin/activate 
+    - Activate virtual environment
+2. python3 bot.py
+    - Run Bot
+3. Type in GroupMe chat
+
+# Features
+- Add, Subtract, Multiply, Divide two numbers at a time
+- Responds to Good Morning and Good Night
+- Reponds to Hello
--- groupme-bot/bot.py
+++ groupme-bot/bot.py
@@ -10,7 +10,7 @@ BOT_ID = os.getenv("BOT_ID")
 GROUP_ID = os.getenv("GROUP_ID")
 ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
 LAST_MESSAGE_ID = None
-
+GIPHY_KEY = os.getenv("GIPHY_KEY")
 
 def send_message(text, attachments=None):
     """Send a message to the group using the bot."""
@@ -38,10 +38,47 @@ def process_message(message):
     """Process and respond to a message."""
     global LAST_MESSAGE_ID
     text = message["text"].lower()
+    #get_url = f"https://api.groupme.com/v3/groups/{GROUP_ID}/messages"
+    #params = {"token": ACCESS_TOKEN}
+    #response = requests.get(get_url, params=params)
 
     # i.e. responding to a specific message (note that this checks if "hello bot" is anywhere in the message, not just the beginning)
-    if "hello bot" in text:
-        send_message("sup")
+    if ("hello bot" in text) and (message["sender_id"] == "87690352"):
+       send_message("sup")
+    elif ("good morning" in text) and (message["sender_type"] != "bot"):
+        send_message("good morning " + message["name"])
+    elif ("good night" in text) and (message["sender_type"] != "bot"):
+        send_message("good night " + message["name"])
+    elif ("*" in text) and (message["sender_id"] == "87690352"):
+        new_text = text.replace(" ", "")
+        op = new_text.find("*")
+        num1 = new_text[0: op]
+        num2 = new_text[op + 1:]
+        sol = int(num1) * int(num2)
+        send_message(text + " = " + str(sol))
+    elif ("-" in text) and (message["sender_id"] == "87690352"):
+        new_text = text.replace(" ", "")
+        op = new_text.find("-")
+        num1 = new_text[0: op]
+        num2 = new_text[op + 1:]
+        sol = int(num1) - int(num2)
+        send_message(text + " = " + str(sol))
+    elif ("+" in text) and (message["sender_id"] == "87690352"):
+        new_text = text.replace(" ", "")
+        op = new_text.find("+")
+        num1 = new_text[0: op]
+        num2 = new_text[op + 1:]
+        sol = int(num1) + int(num2)
+        send_message(text + " = " + str(sol))
+    elif ("/" in text) and (message["sender_id"] == "87690352"):
+        new_text = text.replace(" ", "")
+        op = new_text.find("/")
+        num1 = new_text[0: op]
+        num2 = new_text[op + 1:]
+        sol = int(num1) / int(num2)
+        send_message(text + " = " + str(sol))
+    
+        
 
     LAST_MESSAGE_ID = message["id"]
 
